bb849be833b10482dad4aa0b0ecbc80a3762f518
BHAVESH KUMAR <128747389+thebhaavesh007@users.noreply.github.com>
2023-09-06 22:04:17 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import React from "react";
 import Header from "./Components/Header/Header";
 import styled from "styled-components";
@@ -13,20 +14,6 @@ import HorizontalWrapper from "./Components/HorizontalWrapper";
 import { motion, useScroll, useTransform } from "framer-motion";
 
 export default function Home() {
-  const video = React.useRef<HTMLDivElement>(null);
-
-  const { scrollYProgress } = useScroll({
-    target: video,
-    offset: ["start end", "end start"],
-  });
-
-  const opacity = useTransform(scrollYProgress, [0, 0.65, 1], [1, 1, 0]);
-  const scale = useTransform(
-    scrollYProgress,
-    [0, 0.6, 0.8, 0.9],
-    [1, 0.8, 0.8, 0]
-  );
-
   return (
     <>
       <Header />
@@ -51,44 +38,15 @@ export default function Home() {
         <Fullpage />
 
         <SectionLayout>
-          <HorizontalWrapper height="40rem" direction={1400}>
-            <div className="cards" style={{ right: 0 }}>
-              {cards.map((card, index) => {
-                return (
-                  <Card
-                    key={index}
-                    title={card.title}
-                    description={card.description}
-                    image={card.image}
-                  />
-                );
-              })}
-            </div>
-          </HorizontalWrapper>
+          <TextSection />
         </SectionLayout>
-
+        
         <SectionLayout>
           <TextSection />
         </SectionLayout>
 
-        <SectionLayout>
-          <motion.div
-            className="video"
-            ref={video}
-            style={{
-              opacity,
-              scale,
-            }}
-          >
-            <iframe
-              src="https://www.youtube.com/embed/OuaUjkZhfqQ"
-              title="YouTube video player"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              allowFullScreen
-            ></iframe>
-          </motion.div>
-        </SectionLayout>
-
+        {/* YouTube iframe removed */}
+        
         <SectionLayout>
           <ZoomSection></ZoomSection>
         </SectionLayout>
@@ -114,14 +72,5 @@ const MainStyled = styled.main`
     gap: 4rem;
   }
 
-  .video {
-    padding: 2rem;
-    background-color: #161616;
-    border-radius: 1rem;
-    iframe {
-      border: none;
-      width: 100%;
-      height: 52rem;
-    }
-  }
+  /* Styles for other components... */
 `;
